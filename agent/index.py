9cf09399d5d753529580477ebd087cb0ffc87495
Soumyajit-io <soumyajit010805@gmail.com>
2026-03-01 13:44:08 +0530

    improve index.py

--- agent/index.py
+++ agent/index.py
@@ -1,26 +1,27 @@
 from dotenv import load_dotenv
-from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_qdrant import QdrantVectorStore
 
 load_dotenv()
 
-loader = PyPDFLoader('Monads.pdf')
-docs = loader.load()
+def get_vector(file:str):
 
-text_split = RecursiveCharacterTextSplitter(
-   chunk_size = 500,
-   chunk_overlap = 100 
+   loader = TextLoader(file)
+   docs = loader.load()
+   text_split = RecursiveCharacterTextSplitter(
+      chunk_size = 400,
+      chunk_overlap = 100 
+      )
+   chunks = text_split.split_documents(documents=docs)
+   embedding_model = OpenAIEmbeddings(
+      model="text-embedding-3-small"
+      )
+   vector_store = QdrantVectorStore.from_documents(
+      documents=chunks,
+      embedding=embedding_model,
+      url = "http://localhost:6333",
+      collection_name = "codebase rag1"
    )
-chunks = text_split.split_documents(documents=docs)
-embedding_model = OpenAIEmbeddings(
-   model="text-embedding-3-small"
-   )
-vector_store = QdrantVectorStore.from_documents(
-   documents=chunks,
-   embedding=embedding_model,
-   url = "http://localhost:6333",
-   collection_name = "codebase rag"
-)
-print("Vector Store created successfully")
+   print("Vector Store created successfully")
